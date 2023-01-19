5e13209d99ec907f2d3d7729a6ebed978f4521a4
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-01-19 17:31:43 -0300

    update

---
+++ javascript/main.js
@@ -0,0 +1,45 @@
+
+let cicloPersonagem = 0;
+
+let arrayPersonagens = ["./imagens/naruto.png", "./imagens/kurama.webp",
+                        "./imagens/gyuki.jpg", "./imagens/hashirama.webp",
+                        "./imagens/kakashi.webp", "./imagens/madara.webp",
+                        "./imagens/sakura.webp", "./imagens/sasuke.jpg", 
+                        "./imagens/shikamaru.jpg", "./imagens/tobirama.jpg"];
+
+let arrayNomesDePersonagens = ["Naruto", "Kurama", "Gyuki", "Hashirama", "Kakashi", 
+                            "Madara", "sakura", "Sasuke", "Shikamaru", "Tobirama"]
+
+function start(){
+    window.location.href = "./perguntas.html"
+}
+
+function left(){
+
+    if(cicloPersonagem <= 0 ){
+        cicloPersonagem = 0
+        alert("Fim da lista");
+    }
+    else{
+        cicloPersonagem = cicloPersonagem - 1;
+        atualizaPersonagem();
+    }
+
+}
+function right(){
+    if(cicloPersonagem >= 9){
+        cicloPersonagem = 9;
+        alert("Fim da lista");
+    }
+    else{
+        cicloPersonagem = cicloPersonagem + 1;
+        atualizaPersonagem();
+    }
+}
+
+function atualizaPersonagem(){
+    const VIEWS = document.getElementById('views')
+    const NAME_PERSON = document.getElementById('name-person')
+    VIEWS.innerHTML = `<img src="${arrayPersonagens[cicloPersonagem]}" alt="imagem de anime">`;
+    NAME_PERSON.innerHTML = `<h1>Quiz ${arrayNomesDePersonagens[cicloPersonagem]}</h1>`;
+}
