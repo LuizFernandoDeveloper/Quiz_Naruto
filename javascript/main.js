d369b187c40c4e6f8ded6e8f1688cdef5a75dadf
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-02-19 17:13:25 -0300

    CDN fake

--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,15 @@
 
 let cicloPersonagem = 0;
 
-let arrayPersonagens = ["./imagens/naruto.png", "./imagens/kurama.webp",
-                        "./imagens/gyuki.jpg", "./imagens/hashirama.webp",
-                        "./imagens/kakashi.webp", "./imagens/madara.webp",
-                        "./imagens/sakura.webp", "./imagens/sasuke.jpg", 
-                        "./imagens/shikamaru.jpg", "./imagens/tobirama.jpg"];
+let arrayPersonagens = ["https://sm.ign.com/t/ign_br/screenshot/default/naruto-shippuden_zy11.960.jpg", "https://i0.wp.com/semprefuigeek.com.br/wp-content/uploads/2022/04/kurama-morreu-em-boruto.jpg?fit=1200%2C675&ssl=1",];
 
-let arrayNomesDePersonagens = ["Naruto", "Kurama", "Gyuki", "Hashirama", "Kakashi", 
-                            "Madara", "sakura", "Sasuke", "Shikamaru", "Tobirama"];
+let arrayNomesDePersonagens = ["Naruto", "Kurama"];
 
 function start(){
-    
     window.location.href = "./perguntas.html?" + cicloPersonagem;
 }
 
-    
-
-
 function left(){
-
     if(cicloPersonagem <= 0 ){
         cicloPersonagem = 0
         alert("Fim da lista");
@@ -28,11 +18,11 @@ function left(){
         cicloPersonagem = cicloPersonagem - 1;
         atualizaPersonagem();
     }
-
 }
+
 function right(){
-    if(cicloPersonagem >= 9){
-        cicloPersonagem = 9;
+    if(cicloPersonagem >= 1){
+        cicloPersonagem = 1;
         alert("Fim da lista");
     }
     else{
--- javascript/perguntas.js
+++ javascript/perguntas.js
@@ -166,24 +166,6 @@ function atualizaGames(){
         case 1:
             kurama();
             break
-        case 2:
-            break
-        case 3:
-            break
-        case 4:
-            break
-        case 5:
-            break
-        case 6:
-            break
-        case 7:
-            break
-        case 8:
-            break
-        case 9:
-            break
-        case 10:
-            break
     }
     position++;
 }
