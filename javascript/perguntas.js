6fbdbf324c26118e8447e25767d2937b577f1202
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-02-19 16:49:01 -0300

    naruto

--- javascript/perguntas.js
+++ javascript/perguntas.js
@@ -65,6 +65,57 @@ let kuramaPeruntasRespostas = [
     }
     
 ]
+let narutoPeruntasRespostas = [
+    {
+        "pergunta":"Qual é a cor do cabelo de naruto?",
+        "Resposta-certa":"Amarelo",
+        "resposta-errada":"vermelho",
+        "resposta-errada2":"Verde"
+    },
+    {
+        "pergunta":"Qual é o nome da esposa do  naruto?",
+        "Resposta-certa":"Hinata Hyuga",
+        "resposta-errada":"ino yamanaka",
+        "resposta-errada2":"Sakura Haruno"
+    },
+    {
+        "pergunta":"Qual é o nome do clã do naruto?",
+        "Resposta-certa":"Clã Uzumaki",
+        "resposta-errada":"Clã Akimichi",
+        "resposta-errada2":"Clã Hatake"
+    },
+    {
+        "pergunta":"Qual é o clan da esposa do naruto?",
+        "Resposta-certa":"Clã Hyuga",
+        "resposta-errada":"Clã Uchiha",
+        "resposta-errada2":"Clã Yamanaka"
+    },
+    {
+        "pergunta":"Qual é a técnica mais usada pelo naruto, tanto defensiva quando ofensiva",
+        "Resposta-certa": "clones das sombras",
+        "resposta-errada":"Shinra Tensei",
+        "resposta-errada2":"Chidori"
+    },
+    {
+        "pergunta":"Qual é o melhor ataque do naruto ?",
+        "Resposta-certa": "Rasengan",
+        "resposta-errada":"Clones das sombras",
+        "resposta-errada2":"Chidori"
+    },
+    {
+        "pergunta":"Qual é o ataque mais destrutivo?",
+        "Resposta-certa": "Rasenshuriken",
+        "resposta-errada":"Clones das sombras",
+        "resposta-errada2":"Rasengan"
+    },
+    {
+        "pergunta":"O naruto pode dominar o poder da kurama ?",
+        "Resposta-certa": "Sim",
+        "resposta-errada":"Não",
+        "resposta-errada2":"Todas erradas"
+    }
+    
+]
 
 
 atualizaGames()
@@ -79,6 +130,7 @@ function respostaUm(value){
         atualizaGames();
     }
 }
+
 function respostaDois(value){
     if(respostaCertaDaRodada == value){
         score ++;
@@ -88,6 +140,7 @@ function respostaDois(value){
         atualizaGames();
     }
 }
+
 function respostaTres(value){
     if(respostaCertaDaRodada == value){
         score ++;
@@ -97,6 +150,7 @@ function respostaTres(value){
         atualizaGames();
     }
 }
+
 function atualizaGames(){
     load ++;
     if(load >= 9){
@@ -107,6 +161,7 @@ function atualizaGames(){
     randomOption =  Math.floor(Math.random() * 3);
     switch (parametroPersonagem){
         case 0:
+            naruto();
             break
         case 1:
             kurama();
@@ -132,32 +187,67 @@ function atualizaGames(){
     }
     position++;
 }
+
+
 function pageFinaL(){
     areaQuiz.innerHTML = `  <div class="final-page">
                                 <h1 id="scor"><span>Score:</span> ${score}/8</h1>
                                 <button onclick="pageHome()">Clique aqui para jogar outro Quiz</button>
                             </div>`
 }
+
 function pageHome(){
     window.location.href = "./index.html";
 }
-function kurama(){
-    perguntas.innerHTML = `<p>${kuramaPeruntasRespostas[position].pergunta}</p>`
-    respostaCertaDaRodada = kuramaPeruntasRespostas[position]["Resposta-certa"];
-    sizeArrayPersonagem = kuramaPeruntasRespostas.length
+
+
+
+
+function naruto(){
+
+    perguntas.innerHTML = `<p>${narutoPeruntasRespostas[position].pergunta}</p>`
+    respostaCertaDaRodada = narutoPeruntasRespostas[position]["Resposta-certa"];
+    sizeArrayPersonagem = perguntas.innerHTML = `<p>${narutoPeruntasRespostas[position].pergunta}</p>`
+    respostaCertaDaRodada = narutoPeruntasRespostas[position]["Resposta-certa"];
+    sizeArrayPersonagem = narutoPeruntasRespostas.length
+
     if(randomOption = 0){
-        caseBtns.innerHTML = `<button id="resposta-um" value="${kuramaPeruntasRespostas[position]["Resposta-certa"]}"onclick="respostaUm(this.value)">${kuramaPeruntasRespostas[position]["Resposta-certa"]}</button>
-                            <button id="resposta-doi"  value="${kuramaPeruntasRespostas[position]["resposta-errada"]}" onclick="respostaDois(this.value)">${kuramaPeruntasRespostas[position]["resposta-errada"]}</button>
-                            <button id="resposta-tres" value="${kuramaPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaTres(this.value)">${kuramaPeruntasRespostas[position]["resposta-errada2"]}</button>`;
+        caseBtns.innerHTML = `<button id="resposta-um" value="${narutoPeruntasRespostas[position]["Resposta-certa"]}"onclick="respostaUm(this.value)">${narutoPeruntasRespostas[position]["Resposta-certa"]}</button>
+                            <button id="resposta-doi"  value="${narutoPeruntasRespostas[position]["resposta-errada"]}" onclick="respostaDois(this.value)">${narutoPeruntasRespostas[position]["resposta-errada"]}</button>
+                            <button id="resposta-tres" value="${narutoPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaTres(this.value)">${narutoPeruntasRespostas[position]["resposta-errada2"]}</button>`;
     }
     else if (randomOption = 1){
-        caseBtns.innerHTML = `<button id="resposta-um" value="${kuramaPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaUm(this.value)">  ${kuramaPeruntasRespostas[position]["resposta-errada2"]}</button>
-                            <button id="resposta-doi"  value="${kuramaPeruntasRespostas[position]["Resposta-certa"]}" onclick="respostaDois(this.value)">${kuramaPeruntasRespostas[position]["Resposta-certa"]}</button>
-                            <button id="resposta-tres" value="${kuramaPeruntasRespostas[position]["resposta-errada"]}" onclick="respostaTres(this.value)">${kuramaPeruntasRespostas[position]["resposta-errada"]}</button>`;
+        caseBtns.innerHTML = `<button id="resposta-um" value="${narutoPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaUm(this.value)">  ${narutoPeruntasRespostas[position]["resposta-errada2"]}</button>
+                            <button id="resposta-doi"  value="${narutoPeruntasRespostas[position]["Resposta-certa"]}" onclick="respostaDois(this.value)">${narutoPeruntasRespostas[position]["Resposta-certa"]}</button>
+                            <button id="resposta-tres" value="${narutoPeruntasRespostas[position]["resposta-errada"]}" onclick="respostaTres(this.value)">${narutoPeruntasRespostas[position]["resposta-errada"]}</button>`;
     }
     else{
-        caseBtns.innerHTML = `<button id="resposta-um" value="${kuramaPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaUm(this.value)">  ${kuramaPeruntasRespostas[position]["resposta-errada2"]}</button>
-                            <button id="resposta-doi"  value="${kuramaPeruntasRespostas[position]["resposta-errada"]}"onclick="respostaDois(this.value)"> ${kuramaPeruntasRespostas[position]["resposta-errada"]}</button>
-                            <button id="resposta-tres" value="${kuramaPeruntasRespostas[position]["Resposta-certa"]}" onclick="respostaTres(this.value)">${kuramaPeruntasRespostas[position]["Resposta-certa"]}</button>`
+        caseBtns.innerHTML = `<button id="resposta-um" value="${narutoPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaUm(this.value)">  ${narutoPeruntasRespostas[position]["resposta-errada2"]}</button>
+                            <button id="resposta-doi"  value="${narutoPeruntasRespostas[position]["resposta-errada"]}"onclick="respostaDois(this.value)"> ${narutoPeruntasRespostas[position]["resposta-errada"]}</button>
+                            <button id="resposta-tres" value="${narutoPeruntasRespostas[position]["Resposta-certa"]}" onclick="respostaTres(this.value)">${narutoPeruntasRespostas[position]["Resposta-certa"]}</button>`
+    }
+}
+
+
+function kurama(){
+
+    perguntas.innerHTML = `<p>${narutoPeruntasRespostas[position].pergunta}</p>`
+    respostaCertaDaRodada = kuramaPeruntasRespostas[position]["Resposta-certa"];
+    sizeArrayPersonagem = kuramaPeruntasRespostas.length
+
+    if(randomOption = 0){
+        caseBtns.innerHTML = `<button id="resposta-um" value="${narutoPeruntasRespostas[position]["Resposta-certa"]}"onclick="respostaUm(this.value)">${narutoPeruntasRespostas[position]["Resposta-certa"]}</button>
+                            <button id="resposta-doi"  value="${narutoPeruntasRespostas[position]["resposta-errada"]}" onclick="respostaDois(this.value)">${narutoPeruntasRespostas[position]["resposta-errada"]}</button>
+                            <button id="resposta-tres" value="${narutoPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaTres(this.value)">${narutoPeruntasRespostas[position]["resposta-errada2"]}</button>`;
+    }
+    else if (randomOption = 1){
+        caseBtns.innerHTML = `<button id="resposta-um" value="${narutoPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaUm(this.value)">  ${narutoPeruntasRespostas[position]["resposta-errada2"]}</button>
+                            <button id="resposta-doi"  value="${narutoPeruntasRespostas[position]["Resposta-certa"]}" onclick="respostaDois(this.value)">${narutoPeruntasRespostas[position]["Resposta-certa"]}</button>
+                            <button id="resposta-tres" value="${narutoPeruntasRespostas[position]["resposta-errada"]}" onclick="respostaTres(this.value)">${narutoPeruntasRespostas[position]["resposta-errada"]}</button>`;
+    }
+    else{
+        caseBtns.innerHTML = `<button id="resposta-um" value="${narutoPeruntasRespostas[position]["resposta-errada2"]}" onclick="respostaUm(this.value)">  ${narutoPeruntasRespostas[position]["resposta-errada2"]}</button>
+                            <button id="resposta-doi"  value="${narutoPeruntasRespostas[position]["resposta-errada"]}"onclick="respostaDois(this.value)"> ${narutoPeruntasRespostas[position]["resposta-errada"]}</button>
+                            <button id="resposta-tres" value="${narutoPeruntasRespostas[position]["Resposta-certa"]}" onclick="respostaTres(this.value)">${narutoPeruntasRespostas[position]["Resposta-certa"]}</button>`
     }
 }
